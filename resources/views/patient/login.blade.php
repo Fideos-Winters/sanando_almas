a25d90a35e718543813b6076fac410552a229bf5
Your Name <you@example.com>
2026-04-14 00:03:31 -0600

    se cambio la url de los controladores para la api 2

--- resources/views/patient/login.blade.php
+++ resources/views/patient/login.blade.php
@@ -163,7 +163,7 @@
             </div>
         @endif
 
-<a href="http://127.0.0.1:8000/auth/google/redirect" class="btn-google">
+<a href="http://admin.umbrellastella.com/api/auth/google/redirect" class="btn-google">
     <svg width="20" height="20" viewBox="0 0 18 18" xmlns="http://www.w3.org/2000/svg">
         <path d="M17.64 9.2c0-.637-.057-1.251-.164-1.84H9v3.481h4.844c-.209 1.125-.843 2.078-1.796 2.717v2.258h2.908c1.702-1.567 2.684-3.875 2.684-6.615z" fill="#4285F4"/>
         <path d="M9 18c2.43 0 4.467-.806 5.956-2.184l-2.908-2.258c-.806.54-1.837.86-3.048.86-2.344 0-4.328-1.584-5.036-3.711H.957v2.332A8.997 8.997 0 009 18z" fill="#34A853"/>
